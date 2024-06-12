dc2ed1149b97b4c99176adec2948c993eb701b0a
LotifurShabbir <lotifurshabbir54@gmail.com>
2024-06-12 20:21:24 +0600

    daily done

--- Daily Leetcode/75. Sort Colors.py
+++ Daily Leetcode/75. Sort Colors.py
@@ -8,5 +8,5 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[j] < nums[min]:
                     min = j
-            nums[i], nums[min] = nums[min], nums[i]
+            nums[i], nums[min] = nums[min], nums[i] 
         

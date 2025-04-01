05e8277fa4bfd39f61dba50c95f6064ddbf90858
LotifurShabbir <lotifurshabbir54@gmail.com>
2025-04-01 20:11:47 +0600

    TLE Fixed For 2140

--- Daily Leetcode/2140. Solving Questions With Brainpower.cpp
+++ Daily Leetcode/2140. Solving Questions With Brainpower.cpp
@@ -1,13 +1,21 @@
 class Solution {
     public:
-        int dfs(int i, vector<vector<int>>& questions) {
-            if (i >= questions.size()) return 0;
+        vector<long long> dp;
     
-            return max(dfs(i + 1, questions),
-                       questions[i][0] + dfs(i + 1 + questions[i][1], questions));
+        long long dfs(int i, vector<vector<int>>& questions) {
+            if (i >= questions.size())
+                return 0;
+            if (dp[i] != -1) return dp[i];
+    
+            long long skip = dfs(i + 1, questions);
+            long long solve = questions[i][0] + dfs(i + 1 + questions[i][1], questions);
+    
+            return dp[i] = max(skip, solve);
         }
     
         long long mostPoints(vector<vector<int>>& questions) {
+            int n = questions.size();
+            dp.resize(n, -1);
             return dfs(0, questions);
         }
     };

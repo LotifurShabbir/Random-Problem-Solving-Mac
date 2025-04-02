55f1f71eea28d98670db3a76be066ce8aceefdd1
LotifurShabbir <lotifurshabbir54@gmail.com>
2025-04-02 22:12:51 +0600

    wowwww thats an amazing problem

---
+++ Daily Leetcode/2551. Put Marbles in Bags.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        long long putMarbles(vector<int>& weights, int k) {
+            if (k == 1) {
+                return 0;
+            }
+            
+            vector<int> splits;
+            for (size_t i = 0; i < weights.size() - 1; ++i) {
+                splits.push_back(weights[i] + weights[i + 1]);
+            }
+            
+            sort(splits.begin(), splits.end());
+            
+            int max_score = 0, min_score = 0;
+            int i = k - 1;
+            
+            for (int j = 0; j < i; ++j) {
+                min_score += splits[j];
+                max_score += splits[splits.size() - 1 - j];
+            }
+            
+            return max_score - min_score;
+        }
+    };
